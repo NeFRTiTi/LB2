07de2555f7a2b72d1beabdbd41d01741260b084f
Дмитрий Герасимчик <Gerasimchik_dima@mail.ru>
2020-06-02 22:48:37 +0300

    Комментарий к комиту

---
+++ ЛР2/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace ЛР2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FieldTB = new System.Windows.Forms.TextBox();
+            this.FieldRTB = new System.Windows.Forms.RichTextBox();
+            this.SearchBTN = new System.Windows.Forms.Button();
+            this.ClearBTN = new System.Windows.Forms.Button();
+            this.ReplaceTB = new System.Windows.Forms.TextBox();
+            this.ReplaceBTN = new System.Windows.Forms.Button();
+            this.ReplaceAnswerTB = new System.Windows.Forms.TextBox();
+            this.ReversBTN = new System.Windows.Forms.Button();
+            this.EditorRTB = new System.Windows.Forms.RichTextBox();
+            this.ReversTB = new System.Windows.Forms.TextBox();
+            this.ReplaceFromEditorTB = new System.Windows.Forms.TextBox();
+            this.ReplaceEditorBTN = new System.Windows.Forms.Button();
+            this.ReplaceToEditorTB = new System.Windows.Forms.TextBox();
+            this.FindEditorTB = new System.Windows.Forms.TextBox();
+            this.FindEditorBTN = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.ReplaceEditorFirstTB = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FieldTB
+            // 
+            this.FieldTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FieldTB.Location = new System.Drawing.Point(12, 12);
+            this.FieldTB.Name = "FieldTB";
+            this.FieldTB.Size = new System.Drawing.Size(323, 26);
+            this.FieldTB.TabIndex = 0;
+            this.FieldTB.Text = "5D0";
+            // 
+            // FieldRTB
+            // 
+            this.FieldRTB.Location = new System.Drawing.Point(12, 44);
+            this.FieldRTB.Name = "FieldRTB";
+            this.FieldRTB.Size = new System.Drawing.Size(323, 125);
+            this.FieldRTB.TabIndex = 1;
+            this.FieldRTB.Text = "";
+            // 
+            // SearchBTN
+            // 
+            this.SearchBTN.Location = new System.Drawing.Point(341, 12);
+            this.SearchBTN.Name = "SearchBTN";
+            this.SearchBTN.Size = new System.Drawing.Size(130, 77);
+            this.SearchBTN.TabIndex = 2;
+            this.SearchBTN.Text = "Найти";
+            this.SearchBTN.UseVisualStyleBackColor = true;
+            this.SearchBTN.Click += new System.EventHandler(this.SearchBTN_Click);
+            // 
+            // ClearBTN
+            // 
+            this.ClearBTN.Location = new System.Drawing.Point(341, 95);
+            this.ClearBTN.Name = "ClearBTN";
+            this.ClearBTN.Size = new System.Drawing.Size(130, 77);
+            this.ClearBTN.TabIndex = 3;
+            this.ClearBTN.Text = "Очистить";
+            this.ClearBTN.UseVisualStyleBackColor = true;
+            this.ClearBTN.Click += new System.EventHandler(this.ClearBTN_Click);
+            // 
+            // ReplaceTB
+            // 
+            this.ReplaceTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ReplaceTB.Location = new System.Drawing.Point(12, 175);
+            this.ReplaceTB.Name = "ReplaceTB";
+            this.ReplaceTB.Size = new System.Drawing.Size(323, 26);
+            this.ReplaceTB.TabIndex = 4;
+            this.ReplaceTB.Text = "Defg";
+            // 
+            // ReplaceBTN
+            // 
+            this.ReplaceBTN.Location = new System.Drawing.Point(341, 175);
+            this.ReplaceBTN.Name = "ReplaceBTN";
+            this.ReplaceBTN.Size = new System.Drawing.Size(130, 26);
+            this.ReplaceBTN.TabIndex = 5;
+            this.ReplaceBTN.Text = "Заменить";
+            this.ReplaceBTN.UseVisualStyleBackColor = true;
+            this.ReplaceBTN.Click += new System.EventHandler(this.ReplaceBTN_Click);
+            // 
+            // ReplaceAnswerTB
+            // 
+            this.ReplaceAnswerTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ReplaceAnswerTB.Location = new System.Drawing.Point(12, 207);
+            this.ReplaceAnswerTB.Name = "ReplaceAnswerTB";
+            this.ReplaceAnswerTB.Size = new System.Drawing.Size(323, 26);
+            this.ReplaceAnswerTB.TabIndex = 6;
+            // 
+            // ReversBTN
+            // 
+            this.ReversBTN.Location = new System.Drawing.Point(341, 240);
+            this.ReversBTN.Name = "ReversBTN";
+            this.ReversBTN.Size = new System.Drawing.Size(130, 39);
+            this.ReversBTN.TabIndex = 8;
+            this.ReversBTN.Text = "Заменить порядок слов";
+            this.ReversBTN.UseVisualStyleBackColor = true;
+            this.ReversBTN.Click += new System.EventHandler(this.ReversBTN_Click);
+            // 
+            // EditorRTB
+            // 
+            this.EditorRTB.Location = new System.Drawing.Point(12, 287);
+            this.EditorRTB.Name = "EditorRTB";
+            this.EditorRTB.Size = new System.Drawing.Size(191, 90);
+            this.EditorRTB.TabIndex = 9;
+            this.EditorRTB.Text = "";
+            // 
+            // ReversTB
+            // 
+            this.ReversTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ReversTB.Location = new System.Drawing.Point(12, 245);
+            this.ReversTB.Name = "ReversTB";
+            this.ReversTB.Size = new System.Drawing.Size(323, 26);
+            this.ReversTB.TabIndex = 10;
+            // 
+            // ReplaceFromEditorTB
+            // 
+            this.ReplaceFromEditorTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ReplaceFromEditorTB.Location = new System.Drawing.Point(209, 287);
+            this.ReplaceFromEditorTB.Name = "ReplaceFromEditorTB";
+            this.ReplaceFromEditorTB.Size = new System.Drawing.Size(126, 26);
+            this.ReplaceFromEditorTB.TabIndex = 11;
+            // 
+            // ReplaceEditorBTN
+            // 
+            this.ReplaceEditorBTN.Location = new System.Drawing.Point(418, 305);
+            this.ReplaceEditorBTN.Name = "ReplaceEditorBTN";
+            this.ReplaceEditorBTN.Size = new System.Drawing.Size(53, 26);
+            this.ReplaceEditorBTN.TabIndex = 12;
+            this.ReplaceEditorBTN.Text = "Все";
+            this.ReplaceEditorBTN.UseVisualStyleBackColor = true;
+            this.ReplaceEditorBTN.Click += new System.EventHandler(this.ReplaceEditorBTN_Click);
+            // 
+            // ReplaceToEditorTB
+            // 
+            this.ReplaceToEditorTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ReplaceToEditorTB.Location = new System.Drawing.Point(209, 319);
+            this.ReplaceToEditorTB.Name = "ReplaceToEditorTB";
+            this.ReplaceToEditorTB.Size = new System.Drawing.Size(126, 26);
+            this.ReplaceToEditorTB.TabIndex = 13;
+            // 
+            // FindEditorTB
+            // 
+            this.FindEditorTB.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FindEditorTB.Location = new System.Drawing.Point(209, 351);
+            this.FindEditorTB.Name = "FindEditorTB";
+            this.FindEditorTB.Size = new System.Drawing.Size(126, 26);
+            this.FindEditorTB.TabIndex = 15;
+            this.FindEditorTB.TextChanged += new System.EventHandler(this.TextBox3_TextChanged);
+            // 
+            // FindEditorBTN
+            // 
+            this.FindEditorBTN.Location = new System.Drawing.Point(341, 351);
+            this.FindEditorBTN.Name = "FindEditorBTN";
+            this.FindEditorBTN.Size = new System.Drawing.Size(130, 26);
+            this.FindEditorBTN.TabIndex = 14;
+            this.FindEditorBTN.Text = "Найти";
+            this.FindEditorBTN.UseVisualStyleBackColor = true;
+            this.FindEditorBTN.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // ReplaceEditorFirstTB
+            // 
+            this.ReplaceEditorFirstTB.Location = new System.Drawing.Point(341, 305);
+            this.ReplaceEditorFirstTB.Name = "ReplaceEditorFirstTB";
+            this.ReplaceEditorFirstTB.Size = new System.Drawing.Size(71, 26);
+            this.ReplaceEditorFirstTB.TabIndex = 19;
+            this.ReplaceEditorFirstTB.Text = "Заменить";
+            this.ReplaceEditorFirstTB.UseVisualStyleBackColor = true;
+            this.ReplaceEditorFirstTB.Click += new System.EventHandler(this.ReplaceEditorFirstTB_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 384);
+            this.Controls.Add(this.ReplaceEditorFirstTB);
+            this.Controls.Add(this.FindEditorTB);
+            this.Controls.Add(this.FindEditorBTN);
+            this.Controls.Add(this.ReplaceToEditorTB);
+            this.Controls.Add(this.ReplaceEditorBTN);
+            this.Controls.Add(this.ReplaceFromEditorTB);
+            this.Controls.Add(this.ReversTB);
+            this.Controls.Add(this.EditorRTB);
+            this.Controls.Add(this.ReversBTN);
+            this.Controls.Add(this.ReplaceAnswerTB);
+            this.Controls.Add(this.ReplaceBTN);
+            this.Controls.Add(this.ReplaceTB);
+            this.Controls.Add(this.ClearBTN);
+            this.Controls.Add(this.SearchBTN);
+            this.Controls.Add(this.FieldRTB);
+            this.Controls.Add(this.FieldTB);
+            this.Name = "Form1";
+            this.Text = "ЛР2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox FieldTB;
+        private System.Windows.Forms.RichTextBox FieldRTB;
+        private System.Windows.Forms.Button SearchBTN;
+        private System.Windows.Forms.Button ClearBTN;
+        private System.Windows.Forms.TextBox ReplaceTB;
+        private System.Windows.Forms.Button ReplaceBTN;
+        private System.Windows.Forms.TextBox ReplaceAnswerTB;
+        private System.Windows.Forms.Button ReversBTN;
+        private System.Windows.Forms.RichTextBox EditorRTB;
+        private System.Windows.Forms.TextBox ReversTB;
+        private System.Windows.Forms.TextBox ReplaceFromEditorTB;
+        private System.Windows.Forms.Button ReplaceEditorBTN;
+        private System.Windows.Forms.TextBox ReplaceToEditorTB;
+        private System.Windows.Forms.TextBox FindEditorTB;
+        private System.Windows.Forms.Button FindEditorBTN;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button ReplaceEditorFirstTB;
+    }
+}
+
---
+++ ЛР2/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ЛР2
+{
+    public partial class Form1 : Form
+    {
+        public void texs(out int pp)
+        {
+            pp = 8;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            int g;
+            texs(out g);
+            MessageBox.Show(g.ToString());
+
+            StringBuilder sb = new StringBuilder("Hello World");
+
+
+            //string s = ReplaceTB.Text;
+            //string pattern = @"[b|c|d]+";
+            //string target = " ";
+            //Regex regex = new Regex(pattern);
+            //ReplaceTB.Text = regex.Replace(s, target);
+        }
+
+        private void SearchBTN_Click(object sender, EventArgs e)
+        {
+            string[] array = FieldTB.Text.Split(' ');
+            int[] arr = new int[array.Length];
+            for(int i = 0; i < array.Length; i++)
+            {
+                //arr[i] = Convert.ToInt32(array[i], 16);
+                //FieldRTB.Text = FieldRTB.Text + Convert.ToString(arr[i], 10) + "\r";
+                HexToDec(array[i]);             
+            }
+        }
+        public void HexToDec(string hex)
+        {
+            string number = hex;
+            int dec = 0;
+            bool check = true;
+            for (int i = 0; i < number.Length; i++)
+            {
+                try
+                {
+                    if (number[i].ToString() == "A") { dec += 10 * (int)Math.Pow(16, (number.Length - 1) - i); }
+                    else if (number[i].ToString() == "B") { dec += 11 * (int)Math.Pow(16, (number.Length - 1) - i); }
+                    else if (number[i].ToString() == "C") { dec += 12 * (int)Math.Pow(16, (number.Length - 1) - i); }
+                    else if (number[i].ToString() == "D") { dec += 13 * (int)Math.Pow(16, (number.Length - 1) - i); }
+                    else if (number[i].ToString() == "E") { dec += 14 * (int)Math.Pow(16, (number.Length - 1) - i); }
+                    else if (number[i].ToString() == "F") { dec += 15 * (int)Math.Pow(16, (number.Length - 1) - i); }
+                    else { dec += Convert.ToInt32(number[i].ToString()) * (int)Math.Pow(16, (number.Length - 1) - i); }
+                }
+                catch
+                {
+                    check = false;
+                    break;
+                }
+
+            }
+            if(check)FieldRTB.Text += dec + "\r";
+        }
+        private void ClearBTN_Click(object sender, EventArgs e)
+        {
+            FieldRTB.Text = "";
+        }
+
+        private void ReplaceBTN_Click(object sender, EventArgs e)
+        {
+            ReplaceAnswerTB.Text = "";
+            string s = ReplaceTB.Text;
+            char[] mass = new char[] {'a','e','i','o','q','u','y' };
+            char[] text = new char[s.Length];
+            for(int i = 0; i < s.Length; i++)
+            {
+                text[i] = s[i];
+            }
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < mass.Length; j++)
+                {
+                    if (text[i] == mass[j] && i != text.Length-1)
+                    {
+                        int assci = (int)text[i+1];
+                        if(assci == 122)
+                        {
+                            text[i+1] = Convert.ToChar(97);
+                        }
+                        else if(assci < 122 || assci > 97)
+                        {
+                            text[i+1] = Convert.ToChar(assci + 1);
+                        }                        
+                        break;
+                    }
+                }
+            }
+            for (int i = 0; i < text.Length; i++)
+                ReplaceAnswerTB.Text += text[i].ToString();
+        }
+
+        private void ReversBTN_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (ReversTB.Text.Length != 0)
+                {
+                    string[] mass = ReversTB.Text.Split(' ');
+                    ReversTB.Text = "";
+                    for (int i = mass.Length - 1; i >= 0; i--)
+                    {
+                        ReversTB.Text += mass[i];
+                        if (i != 0) ReversTB.Text += " ";
+                    }
+                }
+                else throw new Exception("Поле пустое, нечего переворачивать!");
+            }
+            catch(Exception message)
+            {
+                MessageBox.Show(message.Message,"Ошибка",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        private void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (EditorRTB.Text.Length == 0) throw new Exception("В поле текстового редактора нет текста(!");
+                if (FindEditorTB.Text.Length == 0) throw new Exception("Поле \"Искомого слова\" не заполнено!");
+                int count = 0;
+                string text = EditorRTB.Text;
+                string rep = FindEditorTB.Text;
+                while (text.IndexOf(rep) != -1)
+                {
+                    int i = text.IndexOf(rep);
+                    EditorRTB.SelectionStart = i+count*rep.Length;
+                    EditorRTB.SelectionLength = FindEditorTB.Text.Length;
+                    EditorRTB.SelectionBackColor = Color.Blue;
+                    text = text.Remove(i, rep.Length);
+                    count++;
+                }
+            }
+            catch (Exception message)
+            {
+                MessageBox.Show(message.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ReplaceEditorBTN_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (EditorRTB.Text.Length == 0) throw new Exception("В поле текстового редактора нет текста(!");
+                if (ReplaceFromEditorTB.Text.Length == 0) throw new Exception("Поле \"Что заменить\" не заполнено!");
+                if (ReplaceToEditorTB.Text.Length == 0) throw new Exception("Поле \"На что заменить\" не заполнено!");
+                EditorRTB.Text = EditorRTB.Text.Replace(ReplaceFromEditorTB.Text, ReplaceToEditorTB.Text);
+            }
+            catch(Exception message)
+            {
+                MessageBox.Show(message.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ReplaceEditorFirstTB_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (EditorRTB.Text.Length == 0) throw new Exception("В поле текстового редактора нет текста(!");
+                if (ReplaceFromEditorTB.Text.Length == 0) throw new Exception("Поле \"Что заменить\" не заполнено!");
+                if (ReplaceToEditorTB.Text.Length == 0) throw new Exception("Поле \"На что заменить\" не заполнено!");
+                int i = EditorRTB.Text.IndexOf(ReplaceFromEditorTB.Text);
+                EditorRTB.Text = EditorRTB.Text.Remove(i, ReplaceFromEditorTB.Text.Length).Insert(i, ReplaceToEditorTB.Text);
+            }
+            catch (Exception message)
+            {
+                MessageBox.Show(message.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
